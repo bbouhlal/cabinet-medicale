cd6af5d5dbb8fd1b9ff0742cc924408034702d9a
b1003439 <b1003439@CAN-S109-PC17.salsa.univ-amu.fr>
2014-11-20 14:03:35 +0100

    Implementation de la classe MedecinM

--- gestioncabinet-coreM/src/miage/gestioncabinet/core/MedecinM.java
+++ gestioncabinet-coreM/src/miage/gestioncabinet/core/MedecinM.java
@@ -2,44 +2,14 @@ package miage.gestioncabinet.core;
 
 import miage.gestioncabinet.api.Medecin;
 
-public class MedecinM implements Medecin{
+public class MedecinM extends UtilisateurM implements Medecin{
 
 	private String rpps;
-	
-	@Override
-	public String getCompte() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getNom() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setNom(String nom) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public String getPrenom() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setPrenom(String prenom) {
-		// TODO Auto-generated method stub
-		
-	}
-
 	@Override
 	public String getRPPS() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.rpps;
 	}
-
+	public void setRPPS(String rpps) {
+		this.rpps = rpps;
+	}
 }
