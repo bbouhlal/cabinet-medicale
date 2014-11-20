ca58981ca652cae16ca4f5cbeeca5e04ed2fd0c9
b1003439 <b1003439@CAN-S109-PC17.salsa.univ-amu.fr>
2014-11-20 13:29:30 +0100

    finir l'implementation de la classe Consultation

--- gestioncabinet-coreM/src/miage/gestioncabinet/core/ConsultationM.java
+++ gestioncabinet-coreM/src/miage/gestioncabinet/core/ConsultationM.java
@@ -1,12 +1,9 @@
 package miage.gestioncabinet.core;
 
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import miage.gestioncabinet.api.Consultation;
-import miage.gestioncabinet.api.ConsultationRemoteService;
-import miage.gestioncabinet.api.GestionCabinetException;
 import miage.gestioncabinet.api.Interaction;
 import miage.gestioncabinet.api.Medecin;
 import miage.gestioncabinet.api.Patient;
@@ -19,77 +16,73 @@ public class ConsultationM implements Consultation{
 	 *Identifiant par défaut 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	
 
-	
-	
+
 	/**
 	 * Date de debut du la consultation
 	 */
 	private Calendar heureDebut;
-	
+
 	/**
 	 * Date de fin de la consultation
 	 */
 	private Calendar heureFin;
-	
-	
+
+
 	/**
 	 * Compte rendu du médecin
 	 */
 	private String compteRendu;
-	
+
 	/**
 	 * La liste des traitements
 	 */
 	private List<Traitement> traitements;
-	
+
 	/***
 	 * La liste des interactions
 	 */
 	private List<Interaction> interactions;
-	
-	/***
-	 * La liste des produits
-	 */
-	private List<Produit>	produits;
-	
+
+
 	/**
 	 * Le médecin effectuant la consultation
 	 */
 	private Medecin medecin;
-	
-	
+
+
 	/***
 	 * Le patient malade
 	 */
 	private Patient patient;
 
-	
-	
-	
-	
-	
+
+
+
 	@Override
 	public int compareTo(Consultation o) {
-		// TODO Auto-generated method stub
-		return 0;
+		//Comaprer les dates de debut
+		Integer compare = this.getDebut().compareTo(o.getDebut());
+		
+		if(compare.equals(0))//Comparer les date de fin
+			compare = this.getFin().compareTo(o.getFin());
+		
+		return compare;
 	}
 	@Override
 	public Patient getPatient() {
 		// Instancier un nouveau patient s'il n'existe pas
 		if (patient==null)
 			patient = new PatientM();
-		
+
 		return patient;
 	}
-	
+
 	@Override
 	public void setPatient(Patient patient) {
 		this.patient = patient;
 	}
-	
+
 	@Override
 	public Medecin getMedecin() {
 		return medecin;
@@ -98,7 +91,7 @@ public class ConsultationM implements Consultation{
 	public void setMedecin(Medecin medecin) {
 		this.medecin = medecin;
 	}
-	
+
 	@Override
 	public Calendar getDebut() {
 		return this.heureDebut;
@@ -115,7 +108,7 @@ public class ConsultationM implements Consultation{
 	public void setFin(Calendar date) {
 		this.heureFin = date;
 	}
-	
+
 	@Override
 	public String getCompteRendu() {
 		return this.compteRendu;
@@ -124,7 +117,7 @@ public class ConsultationM implements Consultation{
 	public void setCompteRendu(String texte) {
 		this.compteRendu = texte;
 	}
-	
+
 	@Override
 	public List<Traitement> getPrescription() {
 		return this.traitements;
@@ -147,7 +140,14 @@ public class ConsultationM implements Consultation{
 	@Override
 	public void setInteractions(List<Interaction> interactions) {
 		this.interactions = interactions;
-		
+
 	}
-	
+
+	@Override
+	public boolean equals(Object o) {
+		return (o instanceof Consultation) 
+				&& this.getDebut().equals(((Consultation) o).getDebut())
+				&& this.getFin().equals(((Consultation) o).getFin());
+	}
+
 }
