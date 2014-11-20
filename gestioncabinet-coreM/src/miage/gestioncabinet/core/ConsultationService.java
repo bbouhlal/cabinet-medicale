632e6d8d60a1e537b8ba7d0c329813423951b745
b1003439 <b1003439@CAN-S109-PC17.salsa.univ-amu.fr>
2014-11-20 13:34:40 +0100

    Implementation de la calsse consultationM

--- gestioncabinet-coreM/src/miage/gestioncabinet/core/ConsultationService.java
+++ gestioncabinet-coreM/src/miage/gestioncabinet/core/ConsultationService.java
@@ -1,59 +1,74 @@
 package miage.gestioncabinet.core;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+
+
+
 
 import miage.gestioncabinet.api.Consultation;
 import miage.gestioncabinet.api.ConsultationRemoteService;
 import miage.gestioncabinet.api.GestionCabinetException;
 import miage.gestioncabinet.api.Produit;
 
+
 public class ConsultationService implements ConsultationRemoteService {
 
 	private Consultation consultation;
+	private List<Consultation> listConsultation;
+	private List<Produit> produits;
 	
+	
+	public ConsultationService(){
+		this.produits = new ArrayList<Produit>();
+	}
 	
 	@Override
 	public Consultation getConsultation() {
-		// TODO Auto-generated method stub
-		return null;
+		if (this.consultation == null)	this.consultation = new ConsultationM();
+		return this.consultation;
 	}
 
 	@Override
 	public void setConsultation(Consultation consultation) {
-		// TODO Auto-generated method stub
-		
+		this.consultation = consultation;
 	}
 
 	@Override
 	public Consultation creerRdv(Calendar date) {
-		// TODO Auto-generated method stub
-		return null;
+		Consultation consultation = new ConsultationM();
+		consultation.setDebut(date);
+		return consultation;
 	}
 
 	@Override
 	public List<Produit> rechercherMedicament(String keyword)
 			throws GestionCabinetException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<Produit> listeProduitKey = new ArrayList<Produit>();
+		
+		for (Produit p : produits){
+			if (p.getNom().toUpperCase().contains(keyword.toUpperCase())) listeProduitKey.add(p);
+		}
+		
+		return listeProduitKey;
 	}
 
 	@Override
 	public void analyserPrescription() throws GestionCabinetException {
-		// TODO Auto-generated method stub
-		
+		this.consultation.getPrescription();
 	}
 
 	@Override
 	public Consultation enregistrer() throws GestionCabinetException {
-		// TODO Auto-generated method stub
-		return null;
+		this.listConsultation.add(this.consultation);
+		return this.consultation;
 	}
 
 	@Override
 	public void supprimer() throws GestionCabinetException {
-		// TODO Auto-generated method stub
-		
+		this.listConsultation.remove(this.consultation);
 	}
 
 }
